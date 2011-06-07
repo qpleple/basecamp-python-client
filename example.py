cf0fb856ab3d3910be2a49af638bb6d3280c5ab4
Quentin Pleplé <quentin.pleple@gmail.com>
2011-06-06 18:58:04 -0700

    Added example

---
+++ example.py
@@ -0,0 +1,15 @@
+# Import ElementTree and the Basecamp wrapper module.
+import elementtree.ElementTree as ET
+from basecamp import Basecamp
+
+bc = Basecamp('https://example.basecamphq.com', 'API_KEY', 'X')
+# or 
+# bc = Basecamp('https://example.basecamphq.com', 'user', 'password')
+
+# Fetch one todo list from its ID
+xml = bc.todo_list(14499317)
+items = ET.fromstring(xml).findall('todo-items/todo-item')
+
+# Let's use the ElementTree API to access data via path expressions:
+for item in items:
+    print element.find("content").text
